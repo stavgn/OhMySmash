cea8e69fbe44a1144b457dce77fc26e7504e2be5
Stav Green <stavgn@gmail.com>
2021-11-16 23:08:26 +0200

    gitignore

--- Commands.cpp
+++ Commands.cpp
@@ -10,40 +10,43 @@
 using namespace std;
 
 #if 0
-#define FUNC_ENTRY()  \
+#define FUNC_ENTRY() \
   cout << __PRETTY_FUNCTION__ << " --> " << endl;
 
-#define FUNC_EXIT()  \
+#define FUNC_EXIT() \
   cout << __PRETTY_FUNCTION__ << " <-- " << endl;
 #else
 #define FUNC_ENTRY()
 #define FUNC_EXIT()
+#define WHITESPACE ' '
 #endif
 
-string _ltrim(const std::string& s)
+string _ltrim(const std::string &s)
 {
   size_t start = s.find_first_not_of(WHITESPACE);
   return (start == std::string::npos) ? "" : s.substr(start);
 }
 
-string _rtrim(const std::string& s)
+string _rtrim(const std::string &s)
 {
   size_t end = s.find_last_not_of(WHITESPACE);
   return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
 
-string _trim(const std::string& s)
+string _trim(const std::string &s)
 {
   return _rtrim(_ltrim(s));
 }
 
-int _parseCommandLine(const char* cmd_line, char** args) {
+int _parseCommandLine(const char *cmd_line, char **args)
+{
   FUNC_ENTRY()
   int i = 0;
   std::istringstream iss(_trim(string(cmd_line)).c_str());
-  for(std::string s; iss >> s; ) {
-    args[i] = (char*)malloc(s.length()+1);
-    memset(args[i], 0, s.length()+1);
+  for (std::string s; iss >> s;)
+  {
+    args[i] = (char *)malloc(s.length() + 1);
+    memset(args[i], 0, s.length() + 1);
     strcpy(args[i], s.c_str());
     args[++i] = NULL;
   }
@@ -52,21 +55,25 @@ int _parseCommandLine(const char* cmd_line, char** args) {
   FUNC_EXIT()
 }
 
-bool _isBackgroundComamnd(const char* cmd_line) {
+bool _isBackgroundComamnd(const char *cmd_line)
+{
   const string str(cmd_line);
   return str[str.find_last_not_of(WHITESPACE)] == '&';
 }
 
-void _removeBackgroundSign(char* cmd_line) {
+void _removeBackgroundSign(char *cmd_line)
+{
   const string str(cmd_line);
   // find last character other than spaces
   unsigned int idx = str.find_last_not_of(WHITESPACE);
   // if all characters are spaces then return
-  if (idx == string::npos) {
+  if (idx == string::npos)
+  {
     return;
   }
   // if the command line does not end with & then return
-  if (cmd_line[idx] != '&') {
+  if (cmd_line[idx] != '&')
+  {
     return;
   }
   // replace the & (background sign) with space and then remove all tailing spaces.
@@ -75,22 +82,25 @@ void _removeBackgroundSign(char* cmd_line) {
   cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
 }
 
-// TODO: Add your implementation for classes in Commands.h 
+// TODO: Add your implementation for classes in Commands.h
 
-SmallShell::SmallShell() {
-// TODO: add your implementation
+SmallShell::SmallShell()
+{
+  // TODO: add your implementation
 }
 
-SmallShell::~SmallShell() {
-// TODO: add your implementation
+SmallShell::~SmallShell()
+{
+  // TODO: add your implementation
 }
 
 /**
 * Creates and returns a pointer to Command class which matches the given command line (cmd_line)
 */
-Command * SmallShell::CreateCommand(const char* cmd_line) {
-	// For example:
-/*
+Command *SmallShell::CreateCommand(const char *cmd_line)
+{
+  // For example:
+  /*
   string cmd_s = _trim(string(cmd_line));
   string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
 
@@ -109,7 +119,8 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
   return nullptr;
 }
 
-void SmallShell::executeCommand(const char *cmd_line) {
+void SmallShell::executeCommand(const char *cmd_line)
+{
   // TODO: Add your implementation here
   // for example:
   // Command* cmd = CreateCommand(cmd_line);
